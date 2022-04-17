bcc5cbb2340e542ee9205c438b463ab73d74bb4b
omrran <omran.mohamed996@gmail.com>
2022-04-17 22:01:23 +0300

    add user controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,8 +30,20 @@ Route::middleware(['guest:web'])->group(function(){
 });
 
 Route::middleware(['auth:web'])->group(function(){
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    // /users
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    //return all users
+    Route::get('/users', [UserController::class, 'users']);
+    //return form to new user 
+    Route::get('/new-user', [UserController::class, 'userForm']);
+    // save user
+    Route::post('/save-user', [UserController::class, 'store']);
+    // delete user
+    Route::get('/user/{id}', [UserController::class, 'delete']);
+    //return form to update user info
+    Route::get('/update-user/{id}', [UserController::class, 'updateForm']);
+    //save new user info
+    Route::get('/update-user/{id}', [UserController::class, 'saveUpdate']);
     // /categories
     // /companies
     // /suppliers
